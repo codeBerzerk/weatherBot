daeeb386d014362ab403424a9ad0faf4f429d0d9
codeBerzerk <alphatech664@gmail.com>
2024-07-24 15:52:23 +0300

    smalll fix

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ function App() {
     const [setCity] = useState('');
     const [weather, setWeather] = useState(null);
 
-    const handleSendToTelegram = useCallback(() => {
-        if (weather) {
-            const message = `
-        Weather in ${weather.name}:
-        Temperature: ${weather.main.temp}°C
-        Humidity: ${weather.main.humidity}%
-        Wind Speed: ${weather.wind.speed} m/s
-        Description: ${weather.weather[0].description}
-      `;
-
-            window.Telegram.WebApp.sendData(message);
-        }
-    }, [weather]);
-
-    useEffect(() => {
-        const tg = window.Telegram.WebApp;
-        tg.ready();
-
-        tg.MainButton.setParams({ text: "Send to Telegram" });
-        tg.MainButton.onClick(handleSendToTelegram);
-
-        return () => {
-            tg.MainButton.offClick(handleSendToTelegram);
-        };
-    }, [weather, handleSendToTelegram]);
+    // const handleSendToTelegram = useCallback(() => {
+    //     if (weather) {
+    //         const message = `
+    //     Weather in ${weather.name}:
+    //     Temperature: ${weather.main.temp}°C
+    //     Humidity: ${weather.main.humidity}%
+    //     Wind Speed: ${weather.wind.speed} m/s
+    //     Description: ${weather.weather[0].description}
+    //   `;
+    //
+    //         window.Telegram.WebApp.sendData(message);
+    //     }
+    // }, [weather]);
+    //
+    // useEffect(() => {
+    //     const tg = window.Telegram.WebApp;
+    //     tg.ready();
+    //
+    //     tg.MainButton.setParams({ text: "Send to Telegram" });
+    //     tg.MainButton.onClick(handleSendToTelegram);
+    //
+    //     return () => {
+    //         tg.MainButton.offClick(handleSendToTelegram);
+    //     };
+    // }, [weather, handleSendToTelegram]);
 
     return (
         <div className="App">

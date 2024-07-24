26db8c476d8c853c0b13964d3bd7614253328ca8
codeBerzerk <alphatech664@gmail.com>
2024-07-24 15:54:01 +0300

    smalll fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,10 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import CitySelector from './Components/CitySelector/CitySelector.jsx';
 import WeatherDisplay from './Components/WeatherDisplay/WeatherDisplay.jsx';
 
 function App() {
     const [setCity] = useState('');
     const [weather, setWeather] = useState(null);
-
-    // const handleSendToTelegram = useCallback(() => {
-    //     if (weather) {
-    //         const message = `
-    //     Weather in ${weather.name}:
-    //     Temperature: ${weather.main.temp}°C
-    //     Humidity: ${weather.main.humidity}%
-    //     Wind Speed: ${weather.wind.speed} m/s
-    //     Description: ${weather.weather[0].description}
-    //   `;
-    //
-    //         window.Telegram.WebApp.sendData(message);
-    //     }
-    // }, [weather]);
-    //
-    // useEffect(() => {
-    //     const tg = window.Telegram.WebApp;
-    //     tg.ready();
-    //
-    //     tg.MainButton.setParams({ text: "Send to Telegram" });
-    //     tg.MainButton.onClick(handleSendToTelegram);
-    //
-    //     return () => {
-    //         tg.MainButton.offClick(handleSendToTelegram);
-    //     };
-    // }, [weather, handleSendToTelegram]);
 
     return (
         <div className="App">
